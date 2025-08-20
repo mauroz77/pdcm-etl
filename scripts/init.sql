6a74d9afac58afdcc3cc98e86f3855a960816f59
Mauricio Martinez Jimenez <mmartinez@ebi.ac.uk>
2025-08-20 14:34:56 +0100

    Create `web_anon` role if it does not exist. This is so postREST can expose the endpoints

--- scripts/init.sql
+++ scripts/init.sql
@@ -3,6 +3,18 @@ CREATE SCHEMA IF NOT EXISTS pdcm_api;
 COMMENT ON SCHEMA pdcm_api IS
   'Patient-derived cancer models (PDCMs) are a powerful oncology research platform for studying tumour biology, mechanisms of drug response and resistance and for testing personalised medicine. Distributed nature of repositories for PDCMs (xenografts, organoids and cell lines) and the use of different metadata standards for describing model''s characteristics make it difficult for researchers to identify suitable PDCM models relevant to specific cancer research questions. PDCM Finder aims to solve this problem by providing harmonized and integrated model attributes to support consistent searching across the originating resources';
 
+-- Create the anonymous role
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'web_anon') THEN
+      CREATE ROLE web_anon NOLOGIN;
+   END IF;
+END
+$$;
+
+-- Give usage on the schema
+GRANT USAGE ON SCHEMA pdcm_api TO web_anon;
+
 DROP TABLE IF EXISTS ethnicity CASCADE;
 
 CREATE TABLE ethnicity (
