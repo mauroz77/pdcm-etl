774fa5343cee02e1159853370a6c5fa3e1c7b544
Mauricio Martinez Jimenez <mmartinez@ebi.ac.uk>
2025-08-20 14:33:57 +0100

    Modify search_index so if histology is null, diagnosis is shown instead. This is to prevent null histologies because of no mapping

--- etl/jobs/transformation/search_index_patient_sample_transformer_job.py
+++ etl/jobs/transformation/search_index_patient_sample_transformer_job.py
@@ -1,7 +1,18 @@
 import sys
 
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import array_except, concat_ws, split, array, lit, col, array_intersect, when, lower, udf
+from pyspark.sql.functions import (
+    array_except,
+    concat_ws,
+    split,
+    array,
+    lit,
+    col,
+    array_intersect,
+    when,
+    lower,
+    udf,
+)
 from pyspark.sql.types import StringType
 
 from etl.constants import Constants
@@ -29,7 +40,7 @@ exclude_top_level_terms = [
     "Cancer",
     "Cancer by Special Category",
     "Cancer by Morphology",
-    "Cancer by Site"
+    "Cancer by Site",
 ]
 
 
@@ -54,13 +65,12 @@ def main(argv):
     patient_sample_df = spark.read.parquet(patient_sample_parquet_path)
     patient_df = spark.read.parquet(patient_parquet_path)
     sample_to_ontology_df = spark.read.parquet(sample_to_ontology_parquet_path)
-    ontology_term_diagnosis_df = spark.read.parquet(ontology_term_diagnosis_parquet_path)
+    ontology_term_diagnosis_df = spark.read.parquet(
+        ontology_term_diagnosis_parquet_path
+    )
 
     search_index_patient_sample_df = transform_search_index_patient_sample(
-        patient_sample_df,
-        patient_df,
-        sample_to_ontology_df,
-        ontology_term_diagnosis_df
+        patient_sample_df, patient_df, sample_to_ontology_df, ontology_term_diagnosis_df
     )
 
     search_index_patient_sample_df.write.mode("overwrite").parquet(output_path)
@@ -68,25 +78,32 @@ def main(argv):
 
 # extends patient_sample_df with diagnosis, tumour, and patient information
 def transform_search_index_patient_sample(
-        patient_sample_df: DataFrame,
-        patient_df: DataFrame,
-        sample_to_ontology_df: DataFrame,
-        ontology_term_diagnosis_df: DataFrame
+    patient_sample_df: DataFrame,
+    patient_df: DataFrame,
+    sample_to_ontology_df: DataFrame,
+    ontology_term_diagnosis_df: DataFrame,
 ) -> DataFrame:
-
     patient_sample_df = get_formatted_patient_sample(patient_sample_df)
     patient_df = get_formatted_patient(patient_df)
-    sample_to_ontology_term_df = extend_sample_with_ontology_data(sample_to_ontology_df, ontology_term_diagnosis_df)
+    sample_to_ontology_term_df = extend_sample_with_ontology_data(
+        sample_to_ontology_df, ontology_term_diagnosis_df
+    )
 
     cond = patient_sample_df.id == sample_to_ontology_term_df.sample_id
-    patient_sample_ext_df = patient_sample_df.join(sample_to_ontology_term_df, on=cond, how='left')
+    patient_sample_ext_df = patient_sample_df.join(
+        sample_to_ontology_term_df, on=cond, how="left"
+    )
 
     cond = patient_sample_df.patient_id == patient_df.patient_internal_id
-    patient_sample_ext_df = patient_sample_ext_df.join(patient_df, on=cond, how='left')
+    patient_sample_ext_df = patient_sample_ext_df.join(patient_df, on=cond, how="left")
 
     bin_age_udf = udf(_bin_age, StringType())
-    patient_sample_ext_df = patient_sample_ext_df.withColumn("patient_age", bin_age_udf("patient_age"))
-    patient_sample_ext_df = patient_sample_ext_df.withColumnRenamed("model_id", "pdcm_model_id")
+    patient_sample_ext_df = patient_sample_ext_df.withColumn(
+        "patient_age", bin_age_udf("patient_age")
+    )
+    patient_sample_ext_df = patient_sample_ext_df.withColumnRenamed(
+        "model_id", "pdcm_model_id"
+    )
     patient_sample_ext_df = get_expected_columns(patient_sample_ext_df)
 
     return patient_sample_ext_df
@@ -96,11 +113,17 @@ def transform_search_index_patient_sample(
 def get_formatted_patient_sample(patient_sample_df: DataFrame) -> DataFrame:
     # Renaming columns
     patient_sample_df = patient_sample_df.withColumnRenamed("grade", "cancer_grade")
-    patient_sample_df = patient_sample_df.withColumnRenamed("grading_system", "cancer_grading_system")
+    patient_sample_df = patient_sample_df.withColumnRenamed(
+        "grading_system", "cancer_grading_system"
+    )
     patient_sample_df = patient_sample_df.withColumnRenamed("stage", "cancer_stage")
-    patient_sample_df = patient_sample_df.withColumnRenamed("staging_system", "cancer_staging_system")
-    patient_sample_df = patient_sample_df.withColumnRenamed("age_in_years_at_collection", "patient_age")
-    
+    patient_sample_df = patient_sample_df.withColumnRenamed(
+        "staging_system", "cancer_staging_system"
+    )
+    patient_sample_df = patient_sample_df.withColumnRenamed(
+        "age_in_years_at_collection", "patient_age"
+    )
+
     return patient_sample_df
 
 
@@ -111,42 +134,60 @@ def get_formatted_patient(patient_df: DataFrame) -> DataFrame:
     patient_df = patient_df.withColumnRenamed("age_category", "patient_age_category")
     patient_df = patient_df.withColumn(
         "patient_sex",
-        when(lower(col("patient_sex")).contains("not"), Constants.NOT_PROVIDED_VALUE).otherwise(
-            lower(col("patient_sex"))
-        ),
+        when(
+            lower(col("patient_sex")).contains("not"), Constants.NOT_PROVIDED_VALUE
+        ).otherwise(lower(col("patient_sex"))),
     )
 
     patient_df = patient_df.select(
-        "id", "patient_sex", "history", "initial_diagnosis", "age_at_initial_diagnosis", "patient_age_category", "provider_name",
-        "patient_ethnicity", "ethnicity_assessment_method", "project_group_name")
+        "id",
+        "patient_sex",
+        "history",
+        "initial_diagnosis",
+        "age_at_initial_diagnosis",
+        "patient_age_category",
+        "provider_name",
+        "patient_ethnicity",
+        "ethnicity_assessment_method",
+        "project_group_name",
+    )
     patient_df = patient_df.withColumnRenamed("id", "patient_internal_id")
     return patient_df
 
 
 def extend_sample_with_ontology_data(
-        sample_to_ontology_df: DataFrame,
-        ontology_term_diagnosis_df: DataFrame
+    sample_to_ontology_df: DataFrame, ontology_term_diagnosis_df: DataFrame
 ) -> DataFrame:
     cond = sample_to_ontology_df.ontology_term_id == ontology_term_diagnosis_df.id
     sample_to_ontology_term_df = sample_to_ontology_df.join(
-        ontology_term_diagnosis_df, on=cond, how='left')
+        ontology_term_diagnosis_df, on=cond, how="left"
+    )
 
     sample_to_ontology_term_df = sample_to_ontology_term_df.withColumn(
         "search_terms",
-        array_except(split(concat_ws("|", "term_name", "ancestors"), "\\|"), array(*map(lit, exclude_top_level_terms)))
+        array_except(
+            split(concat_ws("|", "term_name", "ancestors"), "\\|"),
+            array(*map(lit, exclude_top_level_terms)),
+        ),
     )
     sample_to_ontology_term_df = sample_to_ontology_term_df.withColumn(
         "cancer_system",
         array_intersect(array(*map(lit, cancer_systems)), col("search_terms")),
     )
-    sample_to_ontology_term_df = sample_to_ontology_term_df.withColumn("cancer_system", col("cancer_system").getItem(0))
+    sample_to_ontology_term_df = sample_to_ontology_term_df.withColumn(
+        "cancer_system", col("cancer_system").getItem(0)
+    )
 
     sample_to_ontology_term_df = sample_to_ontology_term_df.withColumn(
         "cancer_system",
-        when(col("cancer_system").isNull(), lit("Unclassified")).otherwise(col("cancer_system"))
+        when(col("cancer_system").isNull(), lit("Unclassified")).otherwise(
+            col("cancer_system")
+        ),
     )
 
-    sample_to_ontology_term_df = sample_to_ontology_term_df.withColumn("histology", col("term_name"))
+    sample_to_ontology_term_df = sample_to_ontology_term_df.withColumn(
+        "histology", col("term_name")
+    )
 
     return sample_to_ontology_term_df
 
@@ -213,7 +254,7 @@ def get_expected_columns(patient_sample_ext_df: DataFrame) -> DataFrame:
         "ancestors",
         "search_terms",
         "cancer_system",
-        Constants.DATA_SOURCE_COLUMN
+        Constants.DATA_SOURCE_COLUMN,
     )
 
 
